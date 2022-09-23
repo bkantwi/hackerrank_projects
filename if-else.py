f3ab2bbce745688187d29281ca3491e3b5eeab37
bkantwi <bernardantwi2010@gmail.com>
2022-09-23 17:56:10 +0000

    if-else complete

--- if-else.py
+++ if-else.py
@@ -1,5 +1,19 @@
 #!/bin/python3
 
+# Task
+# Given an integer, , perform the following conditional actions:
+#
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+# Input Format
+#
+# A single line containing a positive integer, .
+#
+# Constraints
+# 1 <= n <= 100
+
 import math
 import os
 import random
