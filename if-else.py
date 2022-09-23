5d4e976f7a7c3f13a2335eed21c6995b0b6aee0d
bkantwi <bernardantwi2010@gmail.com>
2022-09-23 18:38:17 +0000

    if-else complete

--- if-else.py
+++ if-else.py
@@ -4,9 +4,9 @@
 # Given an integer, , perform the following conditional actions:
 #
 # If  is odd, print Weird
-# If  is even and in the inclusive range of  to , print Not Weird
-# If  is even and in the inclusive range of  to , print Weird
-# If  is even and greater than , print Not Weird
+# If  is even and in the inclusive range of 2 to 5, print Not Weird
+# If  is even and in the inclusive range of 6 to 20, print Weird
+# If  is even and greater than 20, print Not Weird
 # Input Format
 #
 # A single line containing a positive integer, .
@@ -20,18 +20,39 @@ import random
 import re
 import sys
 
-n = random.randint(1, 100)
+# n = random.randint(1, 100)
+#
+# print(n)
+#
+# odd = (n % 2) == 1
+# even = (n % 2) == 0
+#
+# if (n % 2) == 1:
+#     print("Weird")
+#
+# elif ((2 <= n <= 5) & even):
+#     print("Not Weird")
+#
+# elif ((6 <= n <= 20) & even):
+#     print("Weird")
+#
+# elif ((n > 20) & even):
+#     print("Not Weird")
 
-mod = n % 2
 
-if mod > 0:
+# OR
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+num =n%2
+
+if num!=0:
     print("Weird")
-
-if (mod == 0 & n >=2 & n <5):
+elif num ==0 and n in range(2,6):
     print("Not Weird")
-
-if (mod == 0 & n >=6 & n <20):
+elif num == 0 and n in range(6,21):
     print("Weird")
-
-if (mod == 0 & n >20):
+elif num==0 and n >= 20:
     print("Not Weird")
